8baef2c1517f4649a068a7ce6ca0275be92f3951
viky221 <adhi.viky96@gmail.com>
2017-04-18 00:26:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,79 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>	MY FORECAST APP</title>
+	<link rel="stylesheet" type="text/css" href="http://nyekrip.com/demo/nyekrip-template-website-responsive-nyekrip/style/reset.css" />
+	<link rel="stylesheet" type="text/css" href="http://nyekrip.com/demo/nyekrip-template-website-responsive-nyekrip/style/style.css" />
+	<link rel="stylesheet" type="text/css" href="http://nyekrip.com/demo/nyekrip-template-website-responsive-nyekrip/style/media-queries.css" />
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400,400italic,700italic' rel='stylesheet' type='text/css'>
+	
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
 
-<html>
-	<head>
-        <link href="css/style.css" rel="stylesheet">
-        <style type="text/css">
-          body {
-            background-repeat: no-repeat;
-            padding-top: 30px;
-            padding-bottom: 20px;
-          }
-        </style>
-        <link href="css/bootstrap-responsive.css" rel="stylesheet">
-	<title>MY FORECAST APP</title>
-	<h2 align=center>Weather Forecast In Semarang City</h2><br>
-			
-	</head>
-	<body>
-        <div align=center>
-			<form action="cuaca1.php" method="get">
-			<input class="btnForm" type="submit" name="submit" value="weather forecast today"/>
-			</form>
-			<form action="prakiraan.php" method="get">
-			<input class="btnForm" type="submit" name="submit" value="weather forecasts for the next seven days"/>
-			</form>
-		</div>
-	</body>
+<body id="home">
+	<div id="wrapper">
+		
+		<header>
+			<h1><a href="index.php">MY FORECAST APP</a></h1>
+			<h2>My Forecast App <span>-</span> Applications Weather Forecasts For Semarang city</h2>
+			<nav>
+				<a href="index.php">Home</a>
+				<a href="cuaca2.php">weather forecast today</a>
+				<a href="prakiraan.php">weather forecasts for the next seven days</a>
+				<a href="profile.php">Profile</a>
+				<div class="clearfix"></div>
+			</nav>	
+		</header>
+				
+		<section id="main-content">
+			<div id="featured">
+				<h3>Description :</h3>
+				<h4><a href="#">Description of the MY FORECAST APP</a></h4>
+				<p>MY FORECAST APP is a Free App Weather Forecast. Application of the most nice weather forecasts.
 
-<?php
-date_default_timezone_set("Asia/Jakarta");
+Do not let bad weather surprise you! Whether cloudy, rainy or even stormy outside, applications Weather forecast Guide will give you a state and the current weather forecast in your city.This app provides accurate weather information and relevant with attractive icons. Get more information details with just the swipe - you will get an accurate weather forecast for the next times.
+				<br/></p>
+			</div> <!-- END Featured -->
 
-?>
-<script type="text/javascript">
-    var detik = <?php echo date('s'); ?>;
-    var menit = <?php echo date('i'); ?>;
-    var jam   = <?php echo date('H'); ?>;
-     
-    function clock()
-    {
-        if (detik!=0 && detik%60==0) {
-            menit++;
-            detik=0;
-        }
-        second = detik;
-         
-        if (menit!=0 && menit%60==0) {
-            jam++;
-            menit=0;
-        }
-        minute = menit;
-         
-        if (jam!=0 && jam%24==0) {
-            jam=0;
-        }
-        hour = jam;
-         
-        if (detik<10){
-            second='0'+detik;
-        }
-        if (menit<10){
-            minute='0'+menit;
-        }
-         
-        if (jam<10){
-            hour='0'+jam;
-        }
-        waktu = hour+':'+minute+':'+second;
-         
-        document.getElementById("clock").innerHTML = waktu;
-        detik++;
-    }
- 
-    setInterval(clock,1000);
-</script>
- 
-<div style="text-align:center;">
-    <h3>CURRENT TIME IN INDONESIA :
-    <h2 id="clock">
-</div>
+			<hr/>
+			<div id="latest">
+				<section class="left-col">
+					<h3>features  :</h3><br/>
+					<p>Advantages of My Forecast App :</p><br/>
+					<p></p>o> My Forecast App can display the weather forecast for today<br/>
+					<p></p>o> My Forecast App can display the weather forecast for the next seven days<br/>
+					<p></p>o> My Forecast App is easy to use<br/>
+				</section> <!-- END Left Column -->	
+
+				<aside class="sidebar">
+					<h4><a href="#">Arsip</a></h4>
+					<ul>
+						<li><a href="#">April 17 2017</a></li>
+					</ul>
+					<br/>
+					<h4><a href="#">Kategori</a></h4>
+					<ul>
+						<li><a href="#">HTML</a></li>
+						<li><a href="#">CSS</a></li>
+						<li><a href="#">PHP</a></li>
+					</ul>
+					<br/>
+					<h4><a href="#">Sosial</a></h4>
+					<ul>
+						<li><a href="#">Facebook</a></li>
+						<li><a href="#">Twitter</a></li>
+						<li><a href="#">RSS</a></li>
+						<li><a href="#">Google+</a></li>
+					</ul>	
+				</aside>
+			</div> <!-- END Latest -->
+			<div class="clearfix"></div>
+			<hr/>
+			<div id="about">
+				<h3>About My Forecast App</h3>
+				<p>My Forecast App was made to complete the coursework of web service development courses</p>
+			</div>	
+		</section>	
+		<hr/>
+		<footer>
+			<p>&copy; 2017 - MY FORECAST APP</p>
+		</footer>		
+		
+	</div> <!-- END Wrapper -->
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+</body>
 </html>
