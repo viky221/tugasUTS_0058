83fef4040c9eabd829572974075bbfb385eb04a6
viky221 <adhi.viky96@gmail.com>
2017-04-18 00:24:53 +0700

    Update cuaca2.php

--- cuaca2.php
+++ cuaca2.php
@@ -1,4 +1,38 @@
-<center> <?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>	MY FORECAST APP</title>
+	<link rel="stylesheet" type="text/css" href="http://nyekrip.com/demo/nyekrip-template-website-responsive-nyekrip/style/reset.css" />
+	<link rel="stylesheet" type="text/css" href="http://nyekrip.com/demo/nyekrip-template-website-responsive-nyekrip/style/style.css" />
+	<link rel="stylesheet" type="text/css" href="http://nyekrip.com/demo/nyekrip-template-website-responsive-nyekrip/style/media-queries.css" />
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400,400italic,700italic' rel='stylesheet' type='text/css'>
+	
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	</head>
+	
+<body id="home">
+	<div id="wrapper">
+	<header>
+			<h1><a href="index.php">MY FORECAST APP</a></h1>
+			<h2>My Forecast App <span>-</span> Applications Weather Forecasts For Semarang city</h2>
+			<nav>
+				<a href="index.php">Home</a>
+				<a href="cuaca2.php">weather forecast today</a>
+				<a href="prakiraan.php">weather forecasts for the next seven days</a>
+				<a href="profile.php">Profile</a>
+				<div class="clearfix"></div>
+			</nav>	
+		</header>
+		
+<body id="home">
+	<div id="wrapper">
+<?php
    
 
     $json_string = file_get_contents("http://api.wunderground.com/api/a657d7d2ba430b38/conditions/q/ID/mugassari.json");   
@@ -39,14 +73,14 @@
 	echo "<br>";
 	echo "Information :";
     echo "<br>";
-    echo "On the ${local_time} weather in ${lokasi} ${lokasi2} ${cndtn} and temp ${temp}F|C with ${wind} wind degrees";
+    echo "On the ${local_time} weather in ${lokasi} ${lokasi2} ${cndtn} and temp ${temp}F| ${temp1}C with ${wind} wind degrees";
    
     
 	
 
-?></center>
-<br></br>
-	<form action="Index.php" method="get">
-		<input class="btnForm" type="submit" name="submit" value="Selesai"/>
-	</form>
+?>
+</div> <!-- END Wrapper -->
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+</body>
 </html>
