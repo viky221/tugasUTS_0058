66f78a9f53e91fbfa61bb1dba0d447d6dd34c232
viky221 <adhi.viky96@gmail.com>
2017-04-18 00:30:58 +0700

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>	MY FORECAST APP</title>
+	<link rel="stylesheet" type="text/css" href="http://nyekrip.com/demo/nyekrip-template-website-responsive-nyekrip/style/reset.css" />
+	<link rel="stylesheet" type="text/css" href="http://nyekrip.com/demo/nyekrip-template-website-responsive-nyekrip/style/style.css" />
+	<link rel="stylesheet" type="text/css" href="http://nyekrip.com/demo/nyekrip-template-website-responsive-nyekrip/style/media-queries.css" />
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400,400italic,700italic' rel='stylesheet' type='text/css'>
+	
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body id="home">
+	<div id="wrapper">
+		
+		<header>
+			<h1><a href="index.php">MY FORECAST APP</a></h1>
+			<h2>My Forecast App <span>-</span> Applications Weather Forecasts For Semarang city</h2>
+			<nav>
+				<a href="index.php">Home</a>
+				<a href="cuaca2.php">weather forecast today</a>
+				<a href="prakiraan.php">weather forecasts for the next seven days</a>
+				<a href="profile.php">Profile</a>
+				<div class="clearfix"></div>
+			</nav>	
+		</header>
+				
+		<section id="main-content">
+			<div id="featured">
+				<h3>PROFIL MAHASISWA :</h3>
+				<p>NAMA   : ADHI VIKY SUDIANTORO<br/></p>
+				<p>NIM    : 14.01.55.0058<br/></p>
+				<p>MAKUL  : WEB SERVICE DEVELOPMENT<br/></p>
+				<p>KELAS  : PAGI / B1<br/></p>
+			</div> <!-- END Featured -->	
+		
+	</div> <!-- END Wrapper -->
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+</body>
+</html>
